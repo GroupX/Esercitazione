e6c5a2d13df763b84d902a99b5b7c201a685af82
GroupX <d.tosatto@studenti.unibs.it>
2015-04-28 18:17:48 +0200

    Completata (credo) la classe Brano

--- ArchivioCD/src/it/unibs/ing/fp/groupX/esercitazioni/Brano.java
+++ ArchivioCD/src/it/unibs/ing/fp/groupX/esercitazioni/Brano.java
@@ -2,8 +2,30 @@ package it.unibs.ing.fp.groupX.esercitazioni;
 
 public class Brano
 {
-	private String titolo;
-	private String durata;
+	/** Titolo del brano */
+	private String title;
+	/** Durata del brano*/
+	private String lenght;
 	
+	/**
+	 * Costruttore che inizializza il titolo e la durata del brano
+	 * @param title Titolo del brano
+	 * @param lenght Durata del brano
+	 */
+	public Brano (String title, String lenght) 
+	{
+		this.title = title;
+		this.lenght = lenght;
+	}
 	
+	/**
+	 * Metodo che restituisce una stringa descrittiva (titolo del brano e sua durata)
+	 * @return Descrizione basilare del brano
+	 */
+	public String toString ()
+	{
+		String strn = " Titolo: " + title + "\n Durata: " + lenght + "\n";
+		
+		return strn;
+	}
 }
