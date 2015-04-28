3fcae25a6132075d04cf9781a8517d5624645bc2
GroupX <d.tosatto@studenti.unibs.it>
2015-04-28 16:30:16 +0200

    MyUtil implements

--- MyUtil/src/it/unibs/ing/fp/groupX/myutil/IOLib.java
+++ MyUtil/src/it/unibs/ing/fp/groupX/myutil/IOLib.java
@@ -10,6 +10,9 @@ import java.util.Scanner;
  */
 public class IOLib
 {
+	/** Numero inserito in lettura non valido */
+	private static final String LETTURA_NUMERO_NON_VALIDO = "Numero inserito non valido. Riprovare:";
+
 	/**
 	 * Stampa su console una riga di testo
 	 * 
@@ -22,10 +25,24 @@ public class IOLib
 	}
 
 	/**
+	 * Legge da tastiera un numero intero
+	 * 
+	 * @return Numero letto
+	 */
+	@Deprecated
+	public static int readInt ()
+	{
+		Scanner scnr = new Scanner (System.in);
+		int num = scnr.nextInt ();
+		scnr.close();
+		return num;
+	}
+	
+	/**
 	 * Legge da tastiera un numero intero stampado un messaggio
 	 * 
 	 * @param msg
-	 * 			
+	 * 			Messaggio da stampare prima di leggere il numero
 	 * 
 	 * @return Numero letto
 	 */
@@ -37,16 +54,152 @@ public class IOLib
 		scnr.close();
 		return num;
 	}
-
+	
 	/**
-	 * Legga da tastiera un numero double
+	 * Legge da tastiera un numero intero stampado un messaggio e controllando se è maggiore o uguale di un valore
+	 * 
+	 * @param msg
+	 * 			Messaggio da stampare prima di leggere il numero
+	 * @param lowerBound
+	 * 			Valore di soglia inferiore (accettato dalla lettura)
 	 * 
 	 * @return Numero letto
 	 */
+	public static int readInt (String msg, int lowerBound)
+	{
+		Scanner scnr = new Scanner (System.in);
+		int num;
+		do
+		{
+			printLine(msg);
+			num = scnr.nextInt ();
+			if (num < lowerBound)
+			{
+				printLine(LETTURA_NUMERO_NON_VALIDO);
+			}
+		}while (num < lowerBound);
+		
+		scnr.close();
+		return num;
+	}
+	
+	/**
+	 * Legge da tastiera un numero intero stampado un messaggio e controllando se è in un range di valori, estremi inclusi
+	 * 
+	 * @param msg
+	 * 			Messaggio da stampare prima di leggere il numero
+	 * @param lowerBound
+	 * 			Valore di soglia inferiore (accettato dalla lettura)
+	 * @param upperBound
+	 * 			Valore di soglia superiore (accettato dalla lettura)
+	 * 
+	 * @return Numero letto
+	 */
+	public static int readInt (String msg, int lowerBound, int upperBound)
+	{
+		Scanner scnr = new Scanner (System.in);
+		int num;
+		do
+		{
+			printLine(msg);
+			num = scnr.nextInt ();
+			if (num < lowerBound || num > upperBound)
+			{
+				printLine(LETTURA_NUMERO_NON_VALIDO);
+			}
+		}while (num < upperBound || num > upperBound);
+		
+		scnr.close();
+		return num;
+	}
+	
+	/**
+	 * Legge da tastiera un numero double
+	 * 
+	 * @return Numero letto
+	 */
+	@Deprecated
 	public static double readDouble ()
 	{
 		Scanner scnr = new Scanner (System.in);
-		return scnr.nextDouble ();
+		double num = scnr.nextDouble ();
+		scnr.close();
+		return num;
+	}
+	
+	/**
+	 * Legge da tastiera un numero double stampado un messaggio
+	 * 
+	 * @param msg
+	 * 			Messaggio da stampare prima di leggere il numero
+	 * 
+	 * @return Numero letto
+	 */
+	public static double readDouble (String msg)
+	{
+		printLine(msg);
+		Scanner scnr = new Scanner (System.in);
+		double num = scnr.nextDouble ();
+		scnr.close();
+		return num;
+	}
+	
+	/**
+	 * Legge da tastiera un numero double stampado un messaggio e controllando se è maggiore o uguale di un valore
+	 * 
+	 * @param msg
+	 * 			Messaggio da stampare prima di leggere il numero
+	 * @param lowerBound
+	 * 			Valore di soglia inferiore (accettato dalla lettura)
+	 * 
+	 * @return Numero letto
+	 */
+	public static double readDouble (String msg, double lowerBound)
+	{
+		Scanner scnr = new Scanner (System.in);
+		double num;
+		do
+		{
+			printLine(msg);
+			num = scnr.nextDouble ();
+			if (num < lowerBound)
+			{
+				printLine(LETTURA_NUMERO_NON_VALIDO);
+			}
+		}while (num < lowerBound);
+		
+		scnr.close();
+		return num;
+	}
+	
+	/**
+	 * Legge da tastiera un numero double stampado un messaggio e controllando se è in un range di valori, estremi inclusi
+	 * 
+	 * @param msg
+	 * 			Messaggio da stampare prima di leggere il numero
+	 * @param lowerBound
+	 * 			Valore di soglia inferiore (accettato dalla lettura)
+	 * @param upperBound
+	 * 			Valore di soglia superiore (accettato dalla lettura)
+	 * 
+	 * @return Numero letto
+	 */
+	public static double readDouble (String msg, double lowerBound, double upperBound)
+	{
+		Scanner scnr = new Scanner (System.in);
+		double num;
+		do
+		{
+			printLine(msg);
+			num = scnr.nextDouble ();
+			if (num < lowerBound || num > upperBound)
+			{
+				printLine(LETTURA_NUMERO_NON_VALIDO);
+			}
+		}while (num < upperBound || num > upperBound);
+		
+		scnr.close();
+		return num;
 	}
 
 	/**
@@ -57,7 +210,26 @@ public class IOLib
 	public static String readLine ()
 	{
 		Scanner scnr = new Scanner (System.in);
-		return scnr.nextLine ();
+		String line = scnr.nextLine();
+		scnr.close();
+		return line;
 	}
-
+	
+	/**
+	 * Legge da tastiera una stringa (si ferma a \n) stampando a video un messaggio
+	 * 
+	 * @param msg
+	 * 			Messaggio da stampare a video prima della lettura
+	 * 
+	 * @return Stringa letta
+	 */
+	public static String readLine (String msg)
+	{
+		printLine(msg);
+		Scanner scnr = new Scanner (System.in);
+		String line = scnr.nextLine();
+		scnr.close();
+		return line;
+	}
+	
 }
--- MyUtil/src/it/unibs/ing/fp/groupX/myutil/MyMenu.java
+++ MyUtil/src/it/unibs/ing/fp/groupX/myutil/MyMenu.java
@@ -1,6 +1,67 @@
 package it.unibs.ing.fp.groupX.myutil;
 
+import java.util.ArrayList;
+
+/**
+ * Classe generale di gestione menù
+ * 
+ * @author Gruppo X (Manuel Mazzardi, Paolo Pasquali, Davide Tosatto)
+ *
+ */
 public class MyMenu
 {
+	/** Titolo del menù */
+	private String titolo;
+	/** Array dinamico delle voci del menù */
+	private ArrayList<String> voices;
+	
+	/**
+	 * Costruttore generico che inizializza l'elenco delle voci come vuoto
+	 * @param titolo
+	 * 			Titolo del menù
+	 */
+	public MyMenu (String titolo)
+	{
+		this.titolo = titolo;
+		voices = new ArrayList<String>();
+	}
+	
+	/**
+	 * Costruttore che inizializza l'elenco delle voci da un array dinamico
+	 * @param titolo
+	 * 			Titolo del menù
+	 * @param voices 
+	 * 			Elenco delle voci
+	 */
+	public MyMenu (String titolo, ArrayList<String> voices)
+	{
+		this.titolo = titolo;
+		this.voices = voices;
+	}
+	
+	/**
+	 * Costruttore che inizializza l'elenco delle voci da una serie di voci
+	 * @param titolo
+	 * 			Titolo del menù
+	 * @param voice 
+	 * 			Elenco delle voci
+	 */
+	public MyMenu (String titolo, String ... voice)
+	{
+		this(titolo);
+		for(String s : voice)
+		{
+			addVoice(s);
+		}
+	}
+	
+	/**
+	 * Aggiunge una voce al menù
+	 * @param voice Voce da aggiungere
+	 */
+	public void addVoice (String voice)
+	{
+		voices.add(voice);
+	}
 	
 }
