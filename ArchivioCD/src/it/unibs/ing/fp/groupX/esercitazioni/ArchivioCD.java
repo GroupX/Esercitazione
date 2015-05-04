2443add887f137a7b510d8c294c13028cd744e50
GroupX <d.tosatto@studenti.unibs.it>
2015-05-04 16:31:59 +0200

    File

---
+++ ArchivioCD/src/it/unibs/ing/fp/groupX/esercitazioni/ArchivioCD.java
@@ -0,0 +1,77 @@
+package it.unibs.ing.fp.groupX.esercitazioni;
+
+import java.util.Vector;
+
+public class ArchivioCD
+{
+	/** Titolo dell'archivio dei CD */
+	private String title;
+	/** Collezione dei CD in archivio */
+	private Vector<CD> collection;
+	
+	/**
+	 * Costruttore con titolo come parametro
+	 * @param title
+	 * 			Titolo dell'archivio
+	 */
+	public ArchivioCD (String title)
+	{
+		this.title = title;
+		collection = new Vector<CD>();
+	}
+	
+	/**
+	 * Costruttore con titolo e CDs come parametri
+	 * @param title
+	 * 			Titolo dell'archivio
+	 * @param disk
+	 * 			Sequenza di CD da mettere nella collezione
+	 */
+	public ArchivioCD (String title, CD ... disk)
+	{
+		this(title);
+		for (CD d : disk)
+		{
+			collection.add (d);
+		}
+	}
+	
+	/**
+	 * Inserisci un CD (controlla che il titolo sia univoco)
+	 * @param disk
+	 * 			Disco da inserire
+	 */
+	public boolean addCD (CD disk)
+	{
+		for (int i = 0; i < collection.size (); i++)
+		{
+			if (collection.get (i).isTitle (disk.getTitle ()))
+				return false;
+		}
+		collection.add(disk);
+		return true;
+	}
+	
+	/**
+	 * Rimuove l'ultimo CD inserito
+	 */
+	public void removeLastCD ()
+	{
+		collection.remove (collection.size ()-1);
+	}
+	
+	/**
+	 * Rimuove il CD di indice <i>index</i>
+	 * @param index
+	 * 			Indice del CD da rimuovere
+	 */
+	public void removeCD (int index)
+	{
+		collection.remove (index);
+	}
+	
+	public void searchTitleCD (String title)
+	{
+		
+	}
+}
