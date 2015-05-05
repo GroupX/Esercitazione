933bf9511e41a964159831560a383b8c50a744cc
GroupX <d.tosatto@studenti.unibs.it>
2015-05-05 16:45:33 +0200

    Continuata la classe ArchivioMain, aggiunte le varie opzioni del menù e i primi due errori che potrebbero esserci nel caso di nessun CD da visualizzare o rimuovere

--- ArchivioCD/src/it/unibs/ing/fp/groupX/esercitazioni/ArchivioMain.java
+++ ArchivioCD/src/it/unibs/ing/fp/groupX/esercitazioni/ArchivioMain.java
@@ -1,19 +1,128 @@
+
 package it.unibs.ing.fp.groupX.esercitazioni;
+
 import it.unibs.ing.fp.groupX.myutil.*;
 
 public class ArchivioMain
 {
-	private final static String VOCE1 = "Voce1";
-	private final static int INDICE_VOCE1 = 1;
+	private final static String TITOLO = "Benvenuto nell'archivio CD!\n\nSeleziona cosa vuoi fare: ";
+	private final static String [] VOCI = {"Inserisci un nuovo CD", "Visualizza un CD", "Rimuovi un CD", "Visualizza la tua collezione di CD", "Seleziona un brano casuale"};
+	
+	private final static String ARCHIVIO_CD_TITOLO = "Archivio CD";
+	
+	private final static int INSERISCI_CD_VALUE = 1;
+	private final static int VISUALIZZA_CD_VALUE = 2;
+	private final static int RIMUOVI_CD_VALUE = 3;
+	private final static int VISUALIZZA_COLLEZIONE_VALUE = 4;
+	private final static int SELEZIONA_BRANO_CASUALE_VALUE = 5;
+	
+	private final static String TITOLO_CD = "Inserisci il titolo del CD che vuoi inserire";
+	private final static String AUTORE_CD = "Inserisci l'autore del CD che vuoi inserire";
+	private final static String NUMERO_BRANI_CD = "Inserisci il numero di brani del CD";
+	private final static String TITOLO_BRANO = "Inserisci il titolo del brano";
+	private final static String DURATA_BRANO = "Inserisci la durata del brano";
+	
+	private final static String CD_DA_VISUALIZZARE = "Quale CD vuoi visualizzare?";
+	private final static String CD_DA_RIMUOVERE = "Quale CD vuoi rimuovere?";
+	private final static String CONGRATULAZIONI = "Perfetto! CD rimosso senza errori";
+	private final static String BRANI_A_CASO = "Quanti brani vuoi scegliere casualmente?";
+	
+	private final static String ERRORE_1 = "Non c'è nessun CD da visualizzare!";
+	private final static String ERRORE_2 = "Non c'è nessun CD da rimuovere!";
+	
 	public static void main (String[] args)
 	{
-		// TODO Auto-generated method stub
-		//MyMenu mn = new MyMenu("Titolo", "Voce1", "Voce2", "Voce3");
+		MyMenu menu = new MyMenu (TITOLO, VOCI);
+		ArchivioCD archivio = new ArchivioCD (ARCHIVIO_CD_TITOLO);
 		
-		//mn.getChoice ();
+		int scelta;
 		
-		Durata d = IOLib.readDuration("Porco dio:");
-		IOLib.printDuration(d);
+		do
+		{
+			scelta = menu.getChoice();
+		
+			switch (scelta)
+			{
+			case INSERISCI_CD_VALUE:
+				
+				String CDtitle;
+				CDtitle = IOLib.readLine(TITOLO_CD);
+				
+				String CDauthor;
+				CDauthor = IOLib.readLine(AUTORE_CD);
+				
+				CD disk = new CD(CDtitle, CDauthor);
+				
+				int n;
+				n = IOLib.readInt(NUMERO_BRANI_CD);
+				
+				for (int i = 0; i < n; i++)
+				{
+					String titoloBrano;
+					titoloBrano = IOLib.readLine(TITOLO_BRANO);
+					
+					Durata d;
+					d = IOLib.readDuration(DURATA_BRANO);
+					
+					disk.addBrano(titoloBrano, d);
+				}
+				
+				archivio.addCD(disk);
+				
+				break;
+				
+			case VISUALIZZA_CD_VALUE:
+				
+				String cdScelto;
+				cdScelto = IOLib.readLine(CD_DA_VISUALIZZARE);
+				
+				boolean risultatoStampa;
+				risultatoStampa = archivio.print(cdScelto);
+				
+				if (risultatoStampa == false)
+				{
+					IOLib.printLine(ERRORE_1);
+				}
+				
+				break;
+				
+			case RIMUOVI_CD_VALUE:
+				
+				String cdRimosso;
+				cdRimosso = IOLib.readLine(CD_DA_RIMUOVERE);
+				
+				boolean risultato;
+				risultato = archivio.removeCD(cdRimosso);
+				
+				if (risultato == true)
+				{
+					IOLib.printLine(CONGRATULAZIONI);
+				} else IOLib.printLine(ERRORE_2);
+									
+				break;
+				
+			case VISUALIZZA_COLLEZIONE_VALUE:
+				
+				archivio.print();
+				
+				break;
+				
+			case SELEZIONA_BRANO_CASUALE_VALUE:
+				
+				int braniCasuali;
+				braniCasuali = IOLib.readInt(BRANI_A_CASO);
+				
+				Brano [] braniScelti;
+				braniScelti = archivio.getSomeBrano(braniCasuali); 
+				
+				for (Brano b: braniScelti)
+				{
+					IOLib.printLine(b.toString());
+				}
+				
+				break;
+			}
+		} while (scelta != MyMenu.EXIT_VALUE);
 		
 	}
 
