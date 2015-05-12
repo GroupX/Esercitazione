f09e3987991d1ccecc3375a5f385eca7226e7d45
Davide <d.tosatto@studenti.unibs.it>
2015-05-12 12:13:10 +0200

    Inizio classe Medagliere

---
+++ Medagliere/src/it/unibs/ing/fp/groupX/medagliere/Medagliere.java
@@ -0,0 +1,15 @@
+package it.unibs.ing.fp.groupX.medagliere;
+
+import java.util.ArrayList;
+
+public class Medagliere
+{
+ArrayList <Nazione> nazioni = new ArrayList<Nazione>();
+
+	public Medagliere ()
+	{
+		
+	}
+	
+	
+}
