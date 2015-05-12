dee187e8dd795d7a1ebb019eca2969036fb831b0
GroupX <d.tosatto@studenti.unibs.it>
2015-05-12 12:42:42 +0200

    NazioneTest

--- Medagliere/src/it/unibs/ing/fp/groupX/medagliereTest/NazioneTest.java
+++ Medagliere/src/it/unibs/ing/fp/groupX/medagliereTest/NazioneTest.java
@@ -1,6 +1,38 @@
 package it.unibs.ing.fp.groupX.medagliereTest;
 
+import static org.junit.Assert.*;
+import it.unibs.ing.fp.groupX.medagliere.Nazione;
+import junit.framework.Test;
+
 public class NazioneTest
 {
-
+	private final static String NATION_1_NAME = "Italia";
+	private final static String NATION_2_NAME = "Svizzera";
+	
+	@org.junit.Test
+	public void	creationTest () throws Exception
+	{
+		Nazione n = new Nazione (NATION_1_NAME);
+		
+		assertEquals(NATION_1_NAME, n.getNome ());
+	}
+	
+	@org.junit.Test
+	public void equalsTest () throws Exception
+	{
+		Nazione n1 = new Nazione(NATION_1_NAME);
+		Nazione n2 = new Nazione(NATION_1_NAME);
+		Nazione n3 = new Nazione(NATION_2_NAME);
+		
+		assertEquals(true, n1.equals (n2));
+		assertEquals(false, n1.equals (n3));
+	}
+	
+	@org.junit.Test
+	public void toStringTest () throws Exception
+	{
+		Nazione n = new Nazione (NATION_1_NAME);
+		
+		assertEquals(NATION_1_NAME, n.toString ());
+	}
 }
