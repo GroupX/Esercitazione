247cb23fda881d2dcf4acdfc0bfc1acd43b91b3f
GroupX <d.tosatto@studenti.unibs.it>
2015-05-12 12:19:03 +0200

    unpdates Risultato

--- Medagliere/src/it/unibs/ing/fp/groupX/medagliere/Risultato.java
+++ Medagliere/src/it/unibs/ing/fp/groupX/medagliere/Risultato.java
@@ -2,5 +2,31 @@ package it.unibs.ing.fp.groupX.medagliere;
 
 public class Risultato
 {
-
+	private final static int NUM_WINNERS = 3;
+	
+	
+	private Nazione[] winners = new Nazione[NUM_WINNERS];
+	private WinDegree indexs;
+	
+	public Risultato (Nazione gold, Nazione silver, Nazione bronze)
+	{
+		winners[WinDegree.GOLD.getIndex()] = gold;
+		winners[WinDegree.SILVER.getIndex()] = silver;
+		winners[WinDegree.BRONZE.getIndex()] = bronze;
+	}
+	
+	public Nazione getGold ()
+	{
+		return winners[WinDegree.GOLD.getIndex()];
+	}
+	
+	public Nazione getSilver ()
+	{
+		return winners[WinDegree.SILVER.getIndex()];
+	}
+	
+	public Nazione getBronze ()
+	{
+		return winners[WinDegree.BRONZE.getIndex()];
+	}
 }
