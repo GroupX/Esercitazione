b4e0b89a692970cdd5839e82ad2534efe7ad4052
Mazza <m.mazzardi@studenti.unibs.it>
2015-05-12 16:06:01 +0200

    Asgherebro

--- Medagliere/src/it/unibs/ing/fp/groupX/medagliere/MedagliereMain.java
+++ Medagliere/src/it/unibs/ing/fp/groupX/medagliere/MedagliereMain.java
@@ -5,6 +5,14 @@ import it.unibs.ing.fp.groupX.myutil.MyMenu;
 
 public class MedagliereMain
 {
+	private static final String ERRORE_RISULTATO = "Risultato già presente!";
+	private static final String RISULTATO_BRONZO = "Inserisci il nome della nazione che ha ottenuto il bronzo: ";
+	private static final String RISULTATO_ARGENTO = "Inserisci il nome della nazione che ha ottenuto l'argento: ";
+	private static final String RISULTATO_ORO = "Inserisci il nome della nazione che ha ottenuto l'oro: ";
+	private static final String RISULTATO_NOME_GARA = "Inserisci il nome della gara di cui vuoi inserire il risultato: ";
+	private static final String NOME_RISULTATO = "Inserisci risultato per una gara: ";
+	private static final String ERRORE_GARA = "Gara già presente!";
+	private static final String NOME_GARA = "Inserisci il nome della gara: ";
 	private static final String NOME_NAZIONE = "Inserisci il nome della nazione: ";
 	private static final String ERRORE_NAZIONE = "Nazione già presente!";
 	private static final int VISUALIZZA_TUTTO_INDEX = 4;
@@ -47,13 +55,32 @@ public class MedagliereMain
 				
 				case INSERISCI_GARA_INDEX:
 					
+					nome = IOLib.readLine(NOME_GARA);
+					
+					ris = med.addGara(nome);
+					
+					if (!ris)
+						IOLib.printLine(ERRORE_GARA);
+					
 					break;
 					
 				case INSERISCI_RISULTATO_INDEX:
 					
+					nome = IOLib.readLine(NOME_RISULTATO);
+					String nomeOro = IOLib.readLine(RISULTATO_ORO);
+					String nomeArgento = IOLib.readLine(RISULTATO_ARGENTO);
+					String nomeBronzo = IOLib.readLine(RISULTATO_BRONZO);
+					
+					ris = med.addRisultato(nome, nomeOro, nomeArgento, nomeBronzo);
+					
+					if (!ris)
+						IOLib.printLine(ERRORE_RISULTATO);
+					
 					break;
 					
 				case VISUALIZZA_TUTTO_INDEX:
+					
+					IOLib.printLine(med.toStringOrdinato());
 					
 					break;					
 			}
