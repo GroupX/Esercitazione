10fd40eb71e4e90f40659e335ce3c56efb23f8b7
Mazza <m.mazzardi@studenti.unibs.it>
2015-05-12 15:20:00 +0200

    MedagliereTest pro

--- Medagliere/src/it/unibs/ing/fp/groupX/medagliereTest/MedagliereTest.java
+++ Medagliere/src/it/unibs/ing/fp/groupX/medagliereTest/MedagliereTest.java
@@ -1,6 +1,7 @@
 package it.unibs.ing.fp.groupX.medagliereTest;
 
 import static org.junit.Assert.*;
+import it.unibs.ing.fp.groupX.medagliere.Gara;
 import it.unibs.ing.fp.groupX.medagliere.Medagliere;
 
 import org.junit.Test;
@@ -55,5 +56,31 @@ public class MedagliereTest
 					+ "Erebor:  Oro: 0; Argento: 0; Bronzo: 3\n"
 					+ "Azz:  Oro: 1; Argento: 0; Bronzo: 0\n"
 					+ "Gondor:  Oro: 0; Argento: 0; Bronzo: 0\n", m.toString());
+	}
+	
+	@Test
+	public void getGaraTest() throws Exception
+	{
+		Medagliere m = new Medagliere();
+		
+		m.addNazione("Francia");
+		m.addNazione("Germania");
+		m.addNazione("Porto Rico");
+		m.addNazione("Pinco Pallino");
+		
+		m.addGara("Valanga");
+		m.addGara("Sinapsi");
+		m.addGara("Sterminio");
+		m.addGara("Zombie");
+		
+		m.addRisultato("Valanga", "Porto Rico", "Francia", "Germania");
+		m.addRisultato("Sinapsi", "Germania", "Porto Rico", "Pinco Pallino");
+		m.addRisultato("Sterminio", "Pinco Pallino", "Germania", "Francia");
+		m.addRisultato("Zombie", "Francia", "Pinco Pallino", "Porto Rico");
+		
+		assertEquals ("Valanga", m.getGara("Valanga").getNome());
+		assertEquals ("Sterminio", m.getGara("Sterminio").getNome());
+		assertEquals ("Zombie", m.getGara("Zombie").getNome());
+		assertEquals ("Sinapsi", m.getGara("Sinapsi").getNome());
 	}
 }
