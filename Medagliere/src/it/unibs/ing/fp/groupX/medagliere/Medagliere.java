25651c0d05feb7b4dad8be3730a972ee77b5c371
Davide <d.tosatto@studenti.unibs.it>
2015-05-12 13:19:23 +0200

    Pincostrìca

--- Medagliere/src/it/unibs/ing/fp/groupX/medagliere/Medagliere.java
+++ Medagliere/src/it/unibs/ing/fp/groupX/medagliere/Medagliere.java
@@ -22,14 +22,15 @@ private ArrayList<Gara> gare = new ArrayList<Gara>();
 	
 	/**
 	 * Aggiunge una nazione (aggiunge solo se non è già presente)
-	 * @param n Nazione da inserire
+	 * @param nome Nazione da inserire
 	 * @return true: nazione inserita false: nazione già presente
 	 */
-	public boolean addNazione (Nazione n)
+	public boolean addNazione (String nome)
 	{
-		if (!hasNazione(n))
+		
+		if (!hasNazione(nome))
 		{
-			nazioni.add(n);
+			nazioni.add(new Nazione (nome));
 			return true;
 		}
 		else
@@ -40,13 +41,83 @@ private ArrayList<Gara> gare = new ArrayList<Gara>();
 	
 	/**
 	 * Verifica che una nazione sia presente
-	 * @param n Nazione da ricercare
+	 * @param nome Nazione da ricercare
 	 * @return true: nazione presente false: nazione non presente
 	 */
-	public boolean hasNazione (Nazione n)
+	public boolean hasNazione (String nome)
 	{
+		Nazione n = new Nazione(nome);
 		return nazioni.contains(n);
 	}
 	
+	/**
+	 * Verifica che una gara sia presente
+	 * @param nome Gara da ricercare 
+	 * @return true: la gara è presente false: la gara non è presente
+	 */
+	public boolean hasGara (String nome)
+	{
+		Gara g = new Gara(nome);
+		return gare.contains(g);
+	}
+	
+	/**
+	 * Inserisce una gara se non già presente
+	 * @param nome Gara da inserire
+	 * @return true: gara inserita false: gara già presente
+	 */
+	public boolean addGara (String nome)
+	{
+		if (!hasGara(nome))
+		{
+			gare.add(new Gara(nome));
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	/**
+	 * Ritorna una gara dato il nome
+	 * @param nome Nome della gara
+	 * @return Gara trovata
+	 */
+	public Gara getGara (String nome)
+	{
+		Gara gi = new Gara(nome);
+		for (Gara g: gare)
+		{
+			if (g.equals(gi))
+				return g;
+		}
+		return null;
+	}
+
+	/**
+	 * Aggiunge un risultato
+	 * @param nomeGara Gara a cui aggiungere il risultato
+	 * @param nomeNazioneOro Nazione che ha preso l'oro
+	 * @param nomeNazioneArgento Nazione che ha preso l'argento
+	 * @param nomeNazioneBronzo Nazione che ha preso il bronzo
+	 * @return true: risultato aggiunto false: risultato non aggiunto
+	 */
+	public boolean addRisultato (String nomeGara, String nomeNazioneOro, String nomeNazioneArgento, String nomeNazioneBronzo)
+	{
+		if (!hasGara(nomeGara))
+			return false;
+		
+		if (!hasNazione(nomeNazioneOro))
+			return false;
+		
+		if (!hasNazione(nomeNazioneArgento))
+			return false;
+		
+		if (!hasNazione(nomeNazioneBronzo))
+			return false;
+		
+		return getGara(nomeGara).setRisultato(new Risultato(new Nazione(nomeNazioneOro), new Nazione(nomeNazioneOro), new Nazione(nomeNazioneOro)));
+	}
 	
 }
