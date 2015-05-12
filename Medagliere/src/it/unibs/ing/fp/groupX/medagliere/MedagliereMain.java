0e4ee2e32b3dfcd56a342f86b7f6dbaf99c7afd8
Davide <d.tosatto@studenti.unibs.it>
2015-05-12 15:46:00 +0200

    dghjoèihtioàW NASKJHFG Ora fai tu mazzzaaaa sKiAv01!!!1!!11

--- Medagliere/src/it/unibs/ing/fp/groupX/medagliere/MedagliereMain.java
+++ Medagliere/src/it/unibs/ing/fp/groupX/medagliere/MedagliereMain.java
@@ -1,9 +1,12 @@
 package it.unibs.ing.fp.groupX.medagliere;
 
+import it.unibs.ing.fp.groupX.myutil.IOLib;
 import it.unibs.ing.fp.groupX.myutil.MyMenu;
 
 public class MedagliereMain
 {
+	private static final String NOME_NAZIONE = "Inserisci il nome della nazione: ";
+	private static final String ERRORE_NAZIONE = "Nazione già presente!";
 	private static final int VISUALIZZA_TUTTO_INDEX = 4;
 	private static final int INSERISCI_RISULTATO_INDEX = 3;
 	private static final int INSERISCI_GARA_INDEX = 2;
@@ -19,6 +22,9 @@ public class MedagliereMain
 	public static void main(String[] args)
 	{
 		MyMenu m = new MyMenu(MENU_TITOLO, INSERISCI_NAZIONE, INSERISCI_GARA, INSERISCI_RISULTATO, VISUALIZZA_TUTTO);
+		Medagliere med = new Medagliere();
+		String nome;
+		boolean ris;
 		
 		int scelta;
 		
@@ -29,6 +35,13 @@ public class MedagliereMain
 			switch (scelta)
 			{
 				case INSERISCI_NAZIONE_INDEX:
+					
+					nome = IOLib.readLine(NOME_NAZIONE);
+					
+					ris = med.addNazione(nome);
+					
+					if (!ris)
+						IOLib.printLine(ERRORE_NAZIONE);
 					
 					break;
 				
