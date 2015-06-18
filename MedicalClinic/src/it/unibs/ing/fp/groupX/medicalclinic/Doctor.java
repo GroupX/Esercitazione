c5a75582b71e1f5e22b80f83d58b4ed3fc0fc952
Paolo <paolo.pasquali@outlook.it>
2015-06-18 17:07:06 +0200

    piccoli progressi con Doctor

--- MedicalClinic/src/it/unibs/ing/fp/groupX/medicalclinic/Doctor.java
+++ MedicalClinic/src/it/unibs/ing/fp/groupX/medicalclinic/Doctor.java
@@ -1,6 +1,11 @@
 package it.unibs.ing.fp.groupX.medicalclinic;
 
+import it.unibs.ing.fp.groupX.myutil.CodiceFiscale;
+import it.unibs.ing.fp.groupX.myutil.Gender;
+import it.unibs.ing.fp.groupX.myutil.NumeroTelefonico;
+
 import java.sql.Time;
+import java.util.Date;
 import java.util.UUID;
 
 public class Doctor extends StaffMember {
@@ -14,11 +19,26 @@ public class Doctor extends StaffMember {
 	
 	/**
 	 * Costruttore con matrice di boolean per gli orari disponibili
+	 * @param name
+	 * 			nome
+	 * @param surname
+	 * 			cognome
+	 * @param birth
+	 * 			data di nascita
+	 * @param birthPlace
+	 * 			luogo di nascita
+	 * @param gen
+	 * 			genere
+	 * @param num
+	 * 			numero telefonico
+	 * @param cod
+	 * 			codice fiscale
 	 * @param hours
 	 * 			Orari disponibili del dottore
 	 */
-	public Doctor (boolean[][] hours)
+	public Doctor (String name, String surname, Date birth, String birthPlace, Gender gen, NumeroTelefonico num, CodiceFiscale cod, boolean[][] hours)
 	{
+		super(name, surname, birth, birthPlace, gen, num, cod);
 		this.hours = hours.clone();
 		this.albo = generateAlbo();
 	}
@@ -32,9 +52,47 @@ public class Doctor extends StaffMember {
 		return albo;
 	}
 	
-	public void setAvailability (WeekDay startDay, WeekDay endDay, Time startTime, Time endTime)
+	/**
+	 * Imposta la disponibilità del dottore
+	 * @param startDay
+	 * 			giorno di partenza
+	 * @param endDay
+	 * 			giorno di fine
+	 * @param startLapse
+	 * 			periodo di inizio
+	 * @param endLapse
+	 * 			periodo di fine
+	 * @param state
+	 * 			stato da impostare
+	 */
+	public void setAvailability (WeekDay startDay, WeekDay endDay, int startLapse, int endLapse, boolean state)
 	{
-		
+		for (int i = startDay.getValue(); i <= endDay.getValue(); i++)
+		{
+			for (int j = startLapse; j <= endLapse; j++)
+			{
+				hours[j][i] = state;
+			}
+		}
+	}
+	
+	/**
+	 * Imposta la disponibilità del dottore
+	 * @param day
+	 * 			giorno da impostare
+	 * @param startLapse
+	 * 			periodo di inizio
+	 * @param endLapse
+	 * 			periodo di fine
+	 * @param state
+	 * 			stato da impostare
+	 */
+	public void setAvailability (WeekDay day, int startLapse, int endLapse, boolean state)
+	{
+		for (int i = startLapse; i <= endLapse; i++)
+		{
+			hours[i][day.getValue()] = state;
+		}
 	}
 	
 	
--- MedicalClinic/src/it/unibs/ing/fp/groupX/medicalclinic/StaffMember.java
+++ MedicalClinic/src/it/unibs/ing/fp/groupX/medicalclinic/StaffMember.java
@@ -1,6 +1,37 @@
 package it.unibs.ing.fp.groupX.medicalclinic;
 
+import it.unibs.ing.fp.groupX.myutil.CodiceFiscale;
+import it.unibs.ing.fp.groupX.myutil.Gender;
+import it.unibs.ing.fp.groupX.myutil.NumeroTelefonico;
+
+import java.util.Date;
+
+/**
+ * Classe per implementazioni future (esempio aggiunta di altri tipi di dipendenti)
+ * @author Gruppo X (Manuel Mazzardi, Paolo Pasquali, Davide Tosatto)
+ *
+ */
 public class StaffMember extends Person
 {
-
+	/**
+	 * Costruttore che inizializza gli attributi
+	 * @param name
+	 * 			nome
+	 * @param surname
+	 * 			cognome
+	 * @param birth
+	 * 			data di nascita
+	 * @param birthPlace
+	 * 			luogo di nascita
+	 * @param gen
+	 * 			genere
+	 * @param num
+	 * 			numero telefonico
+	 * @param cod
+	 * 			codice fiscale
+	 */
+	public StaffMember (String name, String surname, Date birth, String birthPlace, Gender gen, NumeroTelefonico num, CodiceFiscale cod)
+	{
+		super(name, surname, birth, birthPlace, gen, num, cod);
+	}
 }
