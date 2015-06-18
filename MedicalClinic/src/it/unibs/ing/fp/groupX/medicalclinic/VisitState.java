f04d4aa668564267753b371e64aa11bc7368e282
Paolo <paolo.pasquali@outlook.it>
2015-06-18 15:58:39 +0200

    VisitState

---
+++ MedicalClinic/src/it/unibs/ing/fp/groupX/medicalclinic/VisitState.java
@@ -0,0 +1,36 @@
+package it.unibs.ing.fp.groupX.medicalclinic;
+
+/**
+ * Enum per lo stato della visita
+ * @author Gruppo X (Manuel Mazzardi, Paolo Pasquali, Davide Tosatto)
+ *
+ */
+public enum VisitState {
+	PRENOTATA("prenotata"),
+	CONCLUSA("conclusa"),
+	REFERTATA("refertata"),
+	NON_PRENOTABILE("non prenotabile");
+	
+	/** Stato della visita */
+	private String state;
+	
+	/**
+	 * Costruttore privato
+	 * @param s
+	 * 			stato della visita
+	 */
+	private VisitState (String s)
+	{
+		this.state = s;
+	}
+	
+	/**
+	 * Override di toString
+	 * @return stato della visita
+	 */
+	@Override
+	public String toString ()
+	{
+		return state;
+	}
+}
