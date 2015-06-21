ed9c55458ccbb82e408b7379014abf320f7ce953
Davide <davide.tosatto95@gmail.com>
2015-06-21 14:40:50 +0200

    DataManager per patologie

--- MedicalClinic/src/it/unibs/ing/fp/groupX/medicalclinic/DataManager.java
+++ MedicalClinic/src/it/unibs/ing/fp/groupX/medicalclinic/DataManager.java
@@ -1,6 +1,26 @@
 package it.unibs.ing.fp.groupX.medicalclinic;
 
+import it.unibs.ing.fp.groupX.medicalclinic.pathologies.Pathologies;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class DataManager
 {
+	private static final String PATHOLOGIES_FILE_NAME = "pathologies.dat";
+	
+	public static void savePathologies () throws FileNotFoundException, IOException
+	{
+		File f = new File(PATHOLOGIES_FILE_NAME);
 
+		ObjectOutputStream archivio = new ObjectOutputStream(
+		new BufferedOutputStream(
+		new FileOutputStream(f)));
+
+		archivio.writeObject(Pathologies.get());
+	}
 }
